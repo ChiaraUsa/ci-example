cd48e038a46e21e4f9f440d452a6f362867d460f
chiaraUsa <chiara.valenzuela01@usa.edu.co>
2024-02-23 10:34:12 -0500

    Autoformat

--- test_utils.py
+++ test_utils.py
@@ -1,11 +1,13 @@
 from utils import sumar, restar
 
+
 def test_sumar():
-	assert sumar(2, 3) == 5
-	assert sumar(2, 2) == 4
-	assert sumar(1, 1) == 2
+    assert sumar(2, 3) == 5
+    assert sumar(2, 2) == 4
+    assert sumar(1, 1) == 2
+
 
 def test_restar():
-	assert restar(2, 1) == 1
-	assert restar(4, 2) == 2
-	assert restar(5, 5) == 0
+    assert restar(2, 1) == 1
+    assert restar(4, 2) == 2
+    assert restar(5, 5) == 0
